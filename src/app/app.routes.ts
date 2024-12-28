619c90aa439cd309c935e0bc67b7dfb7f585a281
Sabrina Buenavida <sabrina.buenavida@sigsensetech.com>
2024-12-28 11:14:01 -0300

    Added a layout component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from './auth/login/login.component';
 import {authGuard} from './auth/auth.guard';
 import {HomeComponent} from './home/home.component';
+import {LayoutComponent} from './layout/layout/layout.component';
 
 export const routes: Routes = [
   {
@@ -10,15 +11,36 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: 'home',
-    component: HomeComponent,
-    canActivate: [authGuard],
-  },
-  {
     path: '',
-    redirectTo: '/home',
-    pathMatch: 'full',
+    component: LayoutComponent,
+    canActivate: [authGuard],
+    children: [
+      { path: 'home', component: HomeComponent },
+      // {
+      //   path: 'profile',
+      //   loadComponent: () =>
+      //     import('./profile/profile.component').then((m) => m.ProfileComponent),
+      // },
+      // {
+      //   path: 'settings',
+      //   loadComponent: () =>
+      //     import('./settings/settings.component').then(
+      //       (m) => m.SettingsComponent
+      //     ),
+      // },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+    ],
   },
+  // {
+  //   path: 'home',
+  //   component: HomeComponent,
+  //   canActivate: [authGuard],
+  // },
+  // {
+  //   path: '',
+  //   redirectTo: '/home',
+  //   pathMatch: 'full',
+  // },
   {
     path: '**',
     redirectTo: '/login',
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
-import {BehaviorSubject, from, map, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,12 +13,12 @@ export class AuthService {
       });
   }
 
-  login(email: string, password: string): Observable<any> {
-    return from(this.afAuth.signInWithEmailAndPassword(email, password));
+  login(email: string, password: string) {
+    return this.afAuth.signInWithEmailAndPassword(email, password);
   }
 
-  register(email: string, password: string): Observable<any> {
-    return from(this.afAuth.createUserWithEmailAndPassword(email, password));
+  register(email: string, password: string) {
+    return this.afAuth.createUserWithEmailAndPassword(email, password);
   }
 
   logout() {
