f763a189659a05db39aa27769a8b2dea6f3a4327
Sabrina Buenavida <sabrina.buenavida@sigsensetech.com>
2025-01-05 12:34:26 -0300

    Enabled push notifications.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { initializeApp } from 'firebase/app';
+import { getMessaging, onMessage } from 'firebase/messaging';
+import { firebaseConfig } from 'src/app/firebase-config';
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
@@ -21,6 +24,15 @@ if ('serviceWorker' in navigator) {
       console.error('Error al registrar el Firebase Messaging Service Worker:', error);
     });
 }
+
+const firebaseApp = initializeApp(firebaseConfig);
+console.log('Firebase inicializado:', firebaseApp);
+
+// Opcional: Configura Firebase Messaging
+const messaging = getMessaging(firebaseApp);
+onMessage(messaging, (payload) => {
+  console.log('Mensaje recibido en primer plano:', payload);
+});
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
